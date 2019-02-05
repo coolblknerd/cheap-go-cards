37cf890d7c2a5ff0bfb52e798669a7635ba25910
Reggie Davis <reginald.davis88@gmail.com>
2019-02-04 22:12:36 -0600

    added saveToFile, newDeckFromFile, and shuffle

--- deck.go
+++ deck.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+)
 
 // Create new type of 'deck'
 type deck []string
@@ -28,10 +34,37 @@ func newDeck() deck {
 	return cards
 }
 
-func (d deck) shuffle() {
-
-}
-
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, error := ioutil.ReadFile(filename)
+	if error != nil {
+		fmt.Println(error)
+		os.Exit(1)
+	}
+
+	// Type conversion
+	return deck(strings.Split(string(bs), ","))
+}
+
+func (d deck) shuffle() {
+	for i := range d {
+		newPosition := rand.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
+}
+
+// Type conversion with Go can be achieved by declaring a type and
+// initializing it with the value you want to convert.
+// i.e. - []byte("hello") --> [45 23 54 23 23]
--- main.go
+++ main.go
@@ -6,9 +6,6 @@ func newCard() string {
 
 func main() {
 	cards := newDeck()
-
-	hand, remainingDeck := deal(cards, 7)
-
-	hand.print()
-	remainingDeck.print()
+	cards.shuffle()
+	cards.print()
 }
